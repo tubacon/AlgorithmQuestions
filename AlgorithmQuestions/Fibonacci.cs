6d322a35eb7c841ddc15ab57d06b7a65e9601fef
tubacon <tbakoseoglu@gmail.com>
2024-09-19 20:51:54 +0300

    Add project files.

---
+++ AlgorithmQuestions/Fibonacci.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmQuestions
+{
+    public static class Fibonacci
+    {
+        //Fibonacci Sequence is a series of numbers obtained by adding each number with the previous number
+
+        public static void RunFibonacci()
+        {
+            Console.WriteLine("Please enter a count for array.");
+            int arrayCount = int.Parse(Console.ReadLine() ?? "0");
+
+            if(arrayCount == 0)
+            {
+                Console.WriteLine("Incorrect value for array.");
+                return;
+            }
+
+            int num1 = 1;
+            int num2 = 1;
+            int num3;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+            //1
+            //1
+            //2
+            //3
+            //5
+            //8
+            //13
+
+            for (int i = 1; i <= arrayCount; i++)
+            {
+                num3 = num1 + num2;
+                num1 = num2;
+                num2 = num3; 
+                Console.WriteLine(num3);
+            }
+        }
+    }
+}
---
+++ AlgorithmQuestions/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmQuestions;
+
+Console.WriteLine("Hello, World!");
+
+
+//1. Fibonacci 
+Fibonacci.RunFibonacci();
